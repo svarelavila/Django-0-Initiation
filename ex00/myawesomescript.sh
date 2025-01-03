7de079aa85d238077f5c212cf42c776fdb4afd74
soniavarela <svarelavila@gmail.com>
2025-01-03 19:38:02 +0100

    ex00

--- ex00/myawesomescript.sh
+++ ex00/myawesomescript.sh
@@ -9,11 +9,5 @@ fi
 # Get the final URL after redirects
 final_url=$(curl -Ls -o /dev/null -w %{url_effective} "$1")
 
-# Error checking in curl
-if [ $? -ne 0 ]; then
-  echo "Error: Unable to resolve URL."
-  exit 2
-fi
-
 # Show final URL
 echo "$final_url"
